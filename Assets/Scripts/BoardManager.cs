e5a7e1e204201c01afbb9e9865882f99ee173534
cansucreates <cansu.aysagdic@ogr.iu.edu.tr>
2025-04-02 23:54:48 +0300

    Stored cell data

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -4,32 +4,43 @@ using UnityEngine.Tilemaps;
 
 public class BoardManager : MonoBehaviour
 {
+    // for to get cell data
+    public class CellData
+    {
+        public bool Passable;
+    }
+
     private Tilemap m_Tilemap;
 
     public int Width;
     public int Height;
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
+    private CellData[,] m_BoardData; // 2D array to store cell data
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
-        
+        m_BoardData = new CellData[Width, Height]; // Initialize the 2D array
+
         // Create the border and fill the map with ground tiles
         for (int y = 0; y < Height; y++)
         {
             for (int x = 0; x < Width; x++)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
 
                 if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1) // If we are on the border of the map
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)]; // Randomly select a wall tile
+                    m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)]; // Randomly select a ground tile
+                    m_BoardData[x, y].Passable = true;
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
