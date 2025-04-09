7f8a08e3768da6aac18fe779ce953c4e59610652
cansucreates <caysagdic@gmail.com>
2025-04-09 23:23:03 +0300

    Added UI Label for food count

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements; // For UI Document
 
 public class GameManager : MonoBehaviour
 {
@@ -7,6 +8,8 @@ public class GameManager : MonoBehaviour
     public PlayerController PlayerController;
     public TurnManager TurnManager { get; private set; } // Reference to the TurnManager 
     private int m_CurrentFood = 100; // Current food count
+    public UIDocument UIDoc; // Reference to the UI Document
+    private Label m_FoodLabel; // Reference to the food label in the UI
 
     private void Awake() {
         if (Instance != null) {
@@ -19,6 +22,11 @@ public class GameManager : MonoBehaviour
     
     void Start()
     {
+       m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel"); // Get the food label from the UI Document
+       m_FoodLabel.text = "Food: " + m_CurrentFood; // Set the initial food count in the UI
+
+
+
         TurnManager = new TurnManager();
         TurnManager.onTick += OnTurnHappen; // Subscribe to the onTick event
 
@@ -29,7 +37,7 @@ public class GameManager : MonoBehaviour
 
     public void OnTurnHappen() {
         m_CurrentFood -= 1; // decrease food count by 1 for each turn
-        Debug.Log("Current Food: " + m_CurrentFood);
+        m_FoodLabel.text = "Food: " + m_CurrentFood; // Update the food count in the UI
         if (m_CurrentFood <= 0) {
             Debug.Log("Game Over!"); // Game over if food count is 0
         }
