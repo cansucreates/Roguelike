118c3ee7e1bb46456944d7dad1a9cdd9ea3cda60
cansucreates <caysagdic@gmail.com>
2025-04-11 23:06:23 +0300

    Added multiple obstacles and changed obstacle tiles based on how damaged they are

--- Assets/Scripts/WallObject.cs
+++ Assets/Scripts/WallObject.cs
@@ -4,7 +4,9 @@ using UnityEngine.Tilemaps;
 
 public class WallObject : CellObject
 {
-    public Tile ObstacleTile; // Tile to be used for the wall object
+    public Tile[] ObstacleTile; // Tile to be used for the wall object
+    public Tile[] DamagedObstacleTiles; // Tile to be used for the damaged wall object
+    private int m_SelectedTileIndex; // Index of the selected tile
     public int MaxHealth = 3;
     private int m_HealthPoint; // Health of the wall object
     private Tile m_OriginalTile; // Original tile of the wall object
@@ -14,14 +16,23 @@ public class WallObject : CellObject
         base.Init(cell);
 
         m_HealthPoint = MaxHealth; // Initialize the health point to the maximum health
-
         m_OriginalTile = GameManager.Instance.BoardManager.GetCellTile(cell); // Get the original tile of the cell
-        GameManager.Instance.BoardManager.SetCellTile(cell, ObstacleTile); // Set the tile for the wall object in the board manager
+
+        m_SelectedTileIndex = Random.Range(0, ObstacleTile.Length); // Randomly select a tile index
+        Tile tile = ObstacleTile[m_SelectedTileIndex]; // Get the selected tile
+        GameManager.Instance.BoardManager.SetCellTile(cell, tile); // Set the tile for the wall object in the board manager
     }
 
     public override bool PlayerWantsToEnter()
     {
         m_HealthPoint -= 1;
+
+        if (m_HealthPoint == 1)
+        {
+            Tile damagedTile = DamagedObstacleTiles[m_SelectedTileIndex]; // Get the damaged tile
+            GameManager.Instance.BoardManager.SetCellTile(m_Cell, damagedTile); // Set the tile for the wall object in the board manager
+            return false; // Return false to prevent the player from entering the wall object
+        }
 
         if (m_HealthPoint > 0)
         {
@@ -30,6 +41,6 @@ public class WallObject : CellObject
 
         GameManager.Instance.BoardManager.SetCellTile(m_Cell, m_OriginalTile); // Restore the original tile of the cell
         Destroy(gameObject); // Destroy the wall object
-        return true; // Return true to allow the player to enter the cell
+        return true;
     }
 }
