646cb64a7919cbc2d02fd9c9123332ee5ab32a4e
cansucreates <caysagdic@gmail.com>
2025-04-12 18:20:07 +0300

    Added game over logic and UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,9 @@ public class GameManager : MonoBehaviour
     private int m_CurrentFood = 100; // Current food count
     public UIDocument UIDoc; // Reference to the UI Document
     private Label m_FoodLabel; // Reference to the food label in the UI
-    private int m_CurrentLevel = 1; // Current level of the game
+    private int m_CurrentLevel = 0; // Current level of the game
+    private VisualElement m_GameOverPanel; // Reference to the game over panel in the UI
+    private Label m_GameOverMessage; // Reference to the game over message label in the UI
 
     private void Awake()
     {
@@ -24,6 +26,11 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        m_GameOverPanel = UIDoc.rootVisualElement.Q<VisualElement>("GameOverPanel"); // Get the game over panel from the UI Document
+        m_GameOverMessage = m_GameOverPanel.Q<Label>("GameOverMessage"); // Get the game over message label from the UI Document
+
+        m_GameOverPanel.style.visibility = Visibility.Hidden; // Hide the game over panel at the start
+
         TurnManager = new TurnManager();
         TurnManager.onTick += OnTurnHappen; // Subscribe to the onTick event
 
@@ -43,6 +50,13 @@ public class GameManager : MonoBehaviour
     {
         m_CurrentFood += food; // Increase the food count
         m_FoodLabel.text = "Food: " + m_CurrentFood; // Update the food count in the UI
+
+        if (m_CurrentFood <= 0) // If food count is less than or equal to 0
+        {
+            m_GameOverPanel.style.visibility = Visibility.Visible; // Show the game over panel
+            m_GameOverMessage.text =
+                "Game Over!\n\nYou traveled through " + m_CurrentLevel + " levels"; // Set the game over message
+        }
     }
 
     public void NewLevel()
