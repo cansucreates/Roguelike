c15a3a21747e260fe9104e60ac60b493d353d1c0
cansucreates <caysagdic@gmail.com>
2025-04-12 19:07:39 +0300

    Fixed player movement during Game Over state

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     public BoardManager BoardManager;
     public PlayerController PlayerController;
     public TurnManager TurnManager { get; private set; } // Reference to the TurnManager
-    private int m_CurrentFood = 100; // Current food count
+    private int m_CurrentFood = 20; // Current food count
     public UIDocument UIDoc; // Reference to the UI Document
     private Label m_FoodLabel; // Reference to the food label in the UI
     private int m_CurrentLevel = 0; // Current level of the game
@@ -26,18 +26,15 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        m_GameOverPanel = UIDoc.rootVisualElement.Q<VisualElement>("GameOverPanel"); // Get the game over panel from the UI Document
-        m_GameOverMessage = m_GameOverPanel.Q<Label>("GameOverMessage"); // Get the game over message label from the UI Document
-
-        m_GameOverPanel.style.visibility = Visibility.Hidden; // Hide the game over panel at the start
-
         TurnManager = new TurnManager();
-        TurnManager.onTick += OnTurnHappen; // Subscribe to the onTick event
+        TurnManager.onTick += OnTurnHappen;
 
-        NewLevel(); // Start a new level
+        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel");
 
-        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel"); // Get the food label from the UI Document
-        m_FoodLabel.text = "Food: " + m_CurrentFood; // Set the initial food count in the UI
+        m_GameOverPanel = UIDoc.rootVisualElement.Q<VisualElement>("GameOverPanel");
+        m_GameOverMessage = m_GameOverPanel.Q<Label>("GameOverMessage");
+
+        StartNewGame();
     }
 
     public void OnTurnHappen() // method to handle the turn event
@@ -53,9 +50,12 @@ public class GameManager : MonoBehaviour
 
         if (m_CurrentFood <= 0) // If food count is less than or equal to 0
         {
+            PlayerController.GameOver(); // Call the GameOver method in PlayerController
             m_GameOverPanel.style.visibility = Visibility.Visible; // Show the game over panel
             m_GameOverMessage.text =
-                "Game Over!\n\nYou traveled through " + m_CurrentLevel + " levels"; // Set the game over message
+                "Game Over!\n\nYou traveled through "
+                + m_CurrentLevel
+                + " levels\n\nPress Enter to start again"; // Set the game over message
         }
     }
 
@@ -65,5 +65,20 @@ public class GameManager : MonoBehaviour
         BoardManager.Init(); // Initialize the board manager for the new level
         PlayerController.Spawn(BoardManager, new Vector2Int(1, 1)); // Spawn the player at (1, 1) for the new level
         m_CurrentLevel++; // Increase the level count
+    }
+
+    public void StartNewGame()
+    {
+        m_GameOverPanel.style.visibility = Visibility.Hidden; // Hide the game over panel
+
+        m_CurrentLevel = 0; // Reset the current level count
+        m_CurrentFood = 20; // Reset the food count
+        m_FoodLabel.text = "Food: " + m_CurrentFood; // Update the food count in the UI
+
+        BoardManager.Clean(); // Clean the board for a new game
+        BoardManager.Init(); // Initialize the board manager for the new game
+
+        PlayerController.Init();
+        PlayerController.Spawn(BoardManager, new Vector2Int(1, 1)); // Spawn the player at (1, 1) for the new game
     }
 }
