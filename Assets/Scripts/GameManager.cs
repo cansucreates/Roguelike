eb0ac5375a276432cdfeec74101e3456f4dfb431
cansucreates <caysagdic@gmail.com>
2025-04-12 17:57:16 +0300

    Added new level logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
     private int m_CurrentFood = 100; // Current food count
     public UIDocument UIDoc; // Reference to the UI Document
     private Label m_FoodLabel; // Reference to the food label in the UI
+    private int m_CurrentLevel = 1; // Current level of the game
 
     private void Awake()
     {
@@ -23,14 +24,13 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel"); // Get the food label from the UI Document
-        m_FoodLabel.text = "Food: " + m_CurrentFood; // Set the initial food count in the UI
-
         TurnManager = new TurnManager();
         TurnManager.onTick += OnTurnHappen; // Subscribe to the onTick event
 
-        BoardManager.Init(); // Initialize the board manager
-        PlayerController.Spawn(BoardManager, new Vector2Int(1, 1)); // Spawn the player at (1, 1)
+        NewLevel(); // Start a new level
+
+        m_FoodLabel = UIDoc.rootVisualElement.Q<Label>("FoodLabel"); // Get the food label from the UI Document
+        m_FoodLabel.text = "Food: " + m_CurrentFood; // Set the initial food count in the UI
     }
 
     public void OnTurnHappen() // method to handle the turn event
@@ -43,5 +43,13 @@ public class GameManager : MonoBehaviour
     {
         m_CurrentFood += food; // Increase the food count
         m_FoodLabel.text = "Food: " + m_CurrentFood; // Update the food count in the UI
+    }
+
+    public void NewLevel()
+    {
+        BoardManager.Clean(); // Clean the board for a new level
+        BoardManager.Init(); // Initialize the board manager for the new level
+        PlayerController.Spawn(BoardManager, new Vector2Int(1, 1)); // Spawn the player at (1, 1) for the new level
+        m_CurrentLevel++; // Increase the level count
     }
 }
